9c3348d76c3ce15c5665d1f6a941c9052995da2a
Robin Koning <robin.t.koning@gmail.com>
2017-04-28 12:02:26 +0200

    Remove iostream include

--- src/ui/sdl/event/slot/sdl_mouse_event_slot.cpp
+++ src/ui/sdl/event/slot/sdl_mouse_event_slot.cpp
@@ -2,7 +2,6 @@
 // Created by robin on 4/25/17.
 //
 
-#include <iostream>
 #include "sdl_mouse_event_slot.h"
 
 SDL_MouseEventSlot::SDL_MouseEventSlot() : Slot<sdl_mouse_event_data>() {}
@@ -11,6 +10,5 @@ void SDL_MouseEventSlot::on(sdl_mouse_event_data d) {
     // Example on mouse event callback.
     // You can determine whether the event is a mouse-motion, button-down/up event
     // by checking the data parameter.
-
 }
 
