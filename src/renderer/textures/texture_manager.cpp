6556bb2e328f8656725076b25e59820e186c6b9e
Robin Koning <robin.t.koning@gmail.com>
2017-05-06 14:50:36 +0200

    Load texture directly without creating surface

--- src/renderer/textures/texture_manager.cpp
+++ src/renderer/textures/texture_manager.cpp
@@ -29,27 +29,7 @@ TextureManager *TextureManager::get_instance() {
 
 SDL_Texture *TextureManager::loadTexture(std::string path) {
     path = "res/textures/" + path;
-
-    //The final texture
-    SDL_Texture *newTexture = NULL;
-
-    //Load image at specified path
-
-    SDL_Surface *loadedSurface = IMG_Load(path.c_str());
-    if (loadedSurface == NULL) {
-        std::string s = ("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
-        printf(s.data());
-    } else {
-        //Create texture from surface pixels
-        newTexture = SDL_CreateTextureFromSurface(_renderer, loadedSurface);
-        if (newTexture == NULL) {
-        }
-
-        //Get rid of old loaded surface
-        SDL_FreeSurface(loadedSurface);
-    }
-
-    return newTexture;
+    return IMG_LoadTexture(_renderer, path.c_str());
 }
 
 
