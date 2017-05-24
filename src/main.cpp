f667318790575146955d0b129bcb9a210b7828cc
Robin Koning <robin.t.koning@gmail.com>
2017-05-24 18:50:13 +0200

    Add wavemanager to main

--- src/main.cpp
+++ src/main.cpp
@@ -392,7 +392,7 @@ int main(int argc, char **argv) {
     std::vector<MovingEntityType> possibilities = {
             MovingEntityType::KNIGHT
     };
-    Wave *wave = new Wave(1000, 1000, 1000, 10, 2);
+    Wave *wave = new Wave(wave_pre_stage_time, wave_preparation_time, wave_duration, wave_amount, wave_unit_size);
     wave->set_spawn_possibilities(possibilities);
     wm->set_wave(wave);
     vec2 wave_panel_position = {10, 300}, wave_panel_size = {150, 200};
